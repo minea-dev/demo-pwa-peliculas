159cf5c4d63c9ddac7229f6215409ffd799eb175
minealicer <[milisil@uoc.edu]>
2022-02-28 13:11:52 +0100

    paginator

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 const headers = {
     'Content-Type': 'application/json',
     'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hc3Vza2ZnaXVxbXV3enRzYmRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDU3ODI4NjEsImV4cCI6MTk2MTM1ODg2MX0.xbkOOIQEOzI5kYkf2qhJdnGORcfCKT6dyxffZ5NkJuo',
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hc3Vza2ZnaXVxbXV3enRzYmRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDU3ODI4NjEsImV4cCI6MTk2MTM1ODg2MX0.xbkOOIQEOzI5kYkf2qhJdnGORcfCKT6dyxffZ5NkJuo'
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hc3Vza2ZnaXVxbXV3enRzYmRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDU3ODI4NjEsImV4cCI6MTk2MTM1ODg2MX0.xbkOOIQEOzI5kYkf2qhJdnGORcfCKT6dyxffZ5NkJuo',
+    'Range': '0-9'
 };
 
 
@@ -17,13 +18,26 @@ Vue.createApp({
             isLoading: false,
             peliculasEditables: -1,
             editarNombre: '',
-            editarDuracion: ''
+            editarDuracion: '',
+            numeroResultadosPorPagina: 5,
+            pag: 1
         }
     },
     methods: {
+        getHeader: function() {
+            // Página 1: 0*5 = 0
+            const rangoInicio = (this.pag - 1) * this.numeroResultadosPorPagina;
+            // Rango inicio 0: 0 + 5 = 5  --- DEL 0 AL 5
+            const rangoFinal = rangoInicio + this.numeroResultadosPorPagina;
+            // Clono headers
+            let headersNuevoRango = JSON.parse(JSON.stringify(headers));
+            // Modifico el rango
+            headersNuevoRango.Range = `${rangoInicio}-${rangoFinal}`;
+            return headersNuevoRango;
+        },
         getPeliculas: async function() {
             this.isLoading = true;
-            const fetchPeliculas = await fetch(`${this.APIUrl}?select=*`, { headers });
+            const fetchPeliculas = await fetch(`${this.APIUrl}?select=*`, { headers: this.getHeader() });
             this.peliculas = await fetchPeliculas.json();
             this.isLoading = false;
         },
@@ -33,7 +47,7 @@ Vue.createApp({
             this.verFormAnyadir = false;
             // Hacer el POST a la base de datos
             await fetch(this.APIUrl, {
-                headers: headers,
+                headers: this.getHeader(),
                 method: 'POST',
                 body: JSON.stringify({ "name": this.nuevoNombre, "duration": this.nuevaDuracion })
             });
@@ -49,7 +63,7 @@ Vue.createApp({
             //this.$refs[`pelicula-${id}`][0].remove(); -- No funciona bien
             // Borramos de la base de datos
             await fetch(`${this.APIUrl}?id=eq.${id}`, {
-                headers: headers,
+                headers: this.getHeader(),
                 method: 'DELETE'
             });
             this.getPeliculas();
@@ -68,7 +82,7 @@ Vue.createApp({
         editarPelicula: async function(id) {
             this.peliculasEditables = -1;
             await fetch(`${this.APIUrl}?id=eq.${id}`, {
-                headers: headers,
+                headers: this.getHeader(),
                 method: 'PATCH',
                 body: JSON.stringify({ "name": this.editarNombre, "duration": this.editarDuracion })
             });
@@ -82,6 +96,10 @@ Vue.createApp({
             } else {
                 NProgress.done();
             }
+        },
+        // Cuando haya un cambio dentro de pag...
+        pag(value) {
+            this.getPeliculas();
         }
     },
     mounted: function() {
